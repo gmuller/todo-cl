431456b4b30b7df935e49e973a4ee0c3f49d0b08
Grant Muller <grantmuller@gmail.com>
2011-03-02 14:38:31 -0500

    Adding code to save userId and password to a config file (encrypted). Currently you can either set options, OR write a todo, not both.

---
+++ Options.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+
+namespace Todo
+{
+    class Options
+    {
+        [Option("u", "UserId", Required = false, HelpText = "UserId")]
+        public string username = null;
+
+        [Option("p", "password", HelpText = "password")]
+        public string password = null;
+
+        [Option("t", "todo", HelpText = "todo")]
+        public string todo = null;
+
+        [HelpOption(HelpText = "Display this help screen.")]
+        public string GetUsage()
+        {
+            var usage = new StringBuilder();
+            usage.AppendLine("Todo Command Line");
+            usage.AppendLine("use -u or -p to enter username and password");
+            usage.AppendLine("or just enter a todo");
+            return usage.ToString();
+        }
+    }
+}
--- Todo.cs
+++ Todo.cs
@@ -8,17 +8,49 @@ using Toodledo.Model.API;
 using Toodledo.Client;
 using System.Net;
 using System.Collections.Specialized;
+using CommandLine;
 using System.Configuration;
+using System.Security;
 
 namespace Todo
 {
     public class Todo
     {
-        public string UserID = Properties.Settings.Default.userId;
-        public string password = Properties.Settings.Default.password;
+        public SecureString UserID = DecryptString(Properties.Settings.Default.userId);
+        public SecureString password = DecryptString(Properties.Settings.Default.password);
+        static byte[] entropy = System.Text.Encoding.Unicode.GetBytes("Salt Is Not A Password");
 
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            var options = new Options();
+            ICommandLineParser parser = new CommandLineParser();
+            if (parser.ParseArguments(args, options))
+            {
+                bool optionSet = false;
+                // consume Options type fields
+                if (options.username != null)
+                {
+                    optionSet = true;
+                    Properties.Settings.Default.userId = EncryptString(ToSecureString(options.username));
+                    Properties.Settings.Default.Save();
+                    Console.Out.WriteLine("Successfully Set Username");
+                }
+
+                if (options.password != null)
+                {
+                    optionSet = true;
+                    Properties.Settings.Default.password = EncryptString(ToSecureString(options.password));
+                    Properties.Settings.Default.Save();
+                    Console.Out.WriteLine("Successfully Set Password");
+                }
+
+                if (optionSet)
+                {
+                    Console.Out.WriteLine("Options set. Todo is ready to do it.");
+                    return 0;
+                }
+            }
+
             Todo todo = new Todo();
             StringBuilder builder = new StringBuilder();
             foreach (String arg in args)
@@ -32,6 +64,7 @@ namespace Todo
             } catch (Exception e) {
                 Console.Out.WriteLine(e.Message);
             }
+            return 0;
         }
 
         public void addTask(String taskName)
@@ -49,7 +82,7 @@ namespace Todo
         private Session _session = null;
         public Session Session
         {
-            get { return _session ?? Session.Create(UserID, password, null); }
+            get { return _session ?? Session.Create(ToInsecureString(UserID), ToInsecureString(password), null); }
         }
 
         public ITasks Tasks
@@ -60,6 +93,57 @@ namespace Todo
         public IGeneral General
         {
             get { return (IGeneral)this.Session; }
+        }
+
+        public static string EncryptString(System.Security.SecureString input)
+        {
+            byte[] encryptedData = System.Security.Cryptography.ProtectedData.Protect(
+                System.Text.Encoding.Unicode.GetBytes(ToInsecureString(input)),
+                entropy,
+                System.Security.Cryptography.DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public static SecureString DecryptString(string encryptedData)
+        {
+            try
+            {
+                byte[] decryptedData = System.Security.Cryptography.ProtectedData.Unprotect(
+                    Convert.FromBase64String(encryptedData),
+                    entropy,
+                    System.Security.Cryptography.DataProtectionScope.CurrentUser);
+                return ToSecureString(System.Text.Encoding.Unicode.GetString(decryptedData));
+            }
+            catch
+            {
+                return new SecureString();
+            }
+        }
+
+        public static SecureString ToSecureString(string input)
+        {
+            SecureString secure = new SecureString();
+            foreach (char c in input)
+            {
+                secure.AppendChar(c);
+            }
+            secure.MakeReadOnly();
+            return secure;
+        }
+
+        public static string ToInsecureString(SecureString input)
+        {
+            string returnValue = string.Empty;
+            IntPtr ptr = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(input);
+            try
+            {
+                returnValue = System.Runtime.InteropServices.Marshal.PtrToStringBSTR(ptr);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ZeroFreeBSTR(ptr);
+            }
+            return returnValue;
         }
     }
 }
